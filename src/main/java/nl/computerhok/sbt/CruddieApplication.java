16e1295a098aff8772c8255a6888c7ef44a8c09f
Harry Metske <harry.metske@gmail.com>
2016-10-23 16:47:18 +0200

    initial commit

---
+++ src/main/java/nl/computerhok/sbt/CruddieApplication.java
@@ -0,0 +1,12 @@
+package nl.computerhok.sbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddieApplication.class, args);
+	}
+}
